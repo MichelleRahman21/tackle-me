647b5efc48e20b0fc397c4e200d7defbd0d9da1b
michelleRahman21 <michgomez1807@hotmail.com>
2019-09-12 15:26:01 -0400

    Create migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_09_12_192229) do
+ActiveRecord::Schema.define(version: 2019_09_12_192459) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,8 @@ ActiveRecord::Schema.define(version: 2019_09_12_192229) do
     t.string "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "user_id"
+    t.index ["user_id"], name: "index_categories_on_user_id"
   end
 
   create_table "examples", force: :cascade do |t|
@@ -52,6 +54,7 @@ ActiveRecord::Schema.define(version: 2019_09_12_192229) do
     t.index ["token"], name: "index_users_on_token", unique: true
   end
 
+  add_foreign_key "categories", "users"
   add_foreign_key "examples", "users"
   add_foreign_key "items", "users"
 end
