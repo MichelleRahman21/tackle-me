3bc3d3864460916e10eeb0625bd5d57e6d0425fc
michelleRahman21 <michgomez1807@hotmail.com>
2019-09-12 15:23:37 -0400

    Generate scaffold

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  resources :categories, except: %i[new edit]
   # with this two resources we can skip the custom routes (shortcut)
   resources :items, except: %i[new edit]
   # RESTful routes
